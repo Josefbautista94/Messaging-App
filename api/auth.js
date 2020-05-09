b48f1813049945b6af44daa6684b8fb9af3511c9
AbrahamLara <abrahamlara19@gmail.com>
2020-05-08 21:15:39 -0400

    chore: Completed login endpoint template

--- api/auth.js
+++ api/auth.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (_, res) => {
-  res.send("You've reached auth api endpoint");
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(409).json({ msg: "Please complete fields to continue" });
+  }
+
+  /**
+   * TODO: Use User model to verify if
+   * email and password belong to a user
+   * to authenticate them. Also, Generate token
+   * to create session.
+   */
+
+  res.json({ msg: "Successully logged in" });
 });
 
-router.post("/", (req, res) => {
+router.post("/registration", (req, res) => {
   const { name, email, password, confPass } = req.body;
 
   if (!name || !email || !password || !confPass) {
